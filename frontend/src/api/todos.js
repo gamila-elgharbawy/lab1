d0f2aa7c65f195967f5ee90aa68eb67a00357936
gamila-elgharbawy <gamila.elgharbawy@student.giu-uni.de>
2026-09-15 20:31:36 +0300

    Update todos.js

--- frontend/src/api/todos.js
+++ frontend/src/api/todos.js
@@ -5,7 +5,13 @@ const api = axios.create({
   baseURL: 'http://localhost:3000/api/todos'
 });
 
-export const fetchTodos = () => api.get('/').then(res => res.data);
+export const fetchTodos = async (done) => {
+  const response = await api.get('/', {
+    params: done === 'all' ? {} : { done: done === 'done' }
+  });
+
+  return response.data;
+};
 
 export const createTodo = (title) =>
   api.post('/', { title }).then(res => res.data);
